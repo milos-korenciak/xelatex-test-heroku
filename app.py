3219d66e19a41eb3be54ddec5aa64c5e136cc5e5
Miloššš Korenčiak <milos-korenciak@users.noreply.github.com>
2017-02-20 14:31:44 +0100

    app: bugfix

--- app.py
+++ app.py
@@ -3,23 +3,23 @@ import bottle
 import subprocess
 import os
 
-app = btl.default_app()
+app = bottle.default_app()
 
-@btl.get("/")
+@bottle.get("/")
 def get_index():
     # serve static webpage with JS
     BIN_PATH = os.environ["BIN_PATH"]
     os.chdir(BIN_PATH)
     subprocess.call("xelatex --shell-escape -synctex=1 -interaction=nonstopmode  brano2017-02-09_buildpack.tex", shell=True)
-    btl.response.content_type = 'application/pdf'
-    return btl.static_file("brano2017-02-09_buildpack.pdf", root='.')
+    bottle.response.content_type = 'application/pdf'
+    return bottle.static_file("brano2017-02-09_buildpack.pdf", root='.')
 
 
-@btl.get("/log")
+@bottle.get("/log")
 def get_log():
     # serve logfile
-    btl.response.content_type = 'text/plain; charset=latin9'
-    return btl.static_file("logfile.log", root='/tmp/')
+    bottle.response.content_type = 'text/plain; charset=latin9'
+    return bottle.static_file("logfile.log", root='/tmp/')
 
 if __name__ == '__main__':
-    btl.run(host="", port=8080, debug=True, reloader=True)
+    bottle.run(host="", port=8080, debug=True, reloader=True)
