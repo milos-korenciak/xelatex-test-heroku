d5a3549c79acfb7c4fc06267c4fee782e5011599
babbbrak_poradny <babbbrak_poradny@inmail.sk>
2017-02-21 14:31:36 +0100

    fixed problem with bottle.request.body file-UNlike descriptor

--- app.py
+++ app.py
@@ -38,21 +38,19 @@ def process_tex2pdf():
         os.chdir(tempdir)  # we will work in tempdir
         
         with open(os.path.join(tempdir, "sample.tex"), "wb") as sample_tex:
-            while True:  # save the .tex file
-                buf = bottle.request.body.read(4 * 2 ** 10)  # 4 kilobytes at once
-                if not buf: break
-                sample_tex.write(buf)
+            buf = bottle.request.body.read()
+            sample_tex.write(buf)
         print("I have written the sample.tex")
         
         BIN_PATH = os.environ.get("BIN_PATH", "/app/buildpack/bin/x86_64-linux/")  # we search for the xelatex binary 
         XELATEX_PATH = os.path.join(BIN_PATH, "xelatex")
         print("I have XELATEX_PATH", XELATEX_PATH, " BIN_PATH", BIN_PATH)
         
-        # subprocess.call(XELATEX_PATH + " --shell-escape -synctex=1 -interaction=nonstopmode sample.tex", shell=True)
-        # bottle.response.content_type = 'application/pdf'
-        bottle.response.content_type = 'text/plain'
-        print("returning the sample.tex")
-        return bottle.static_file("sample.tex", root=tempdir)
+        subprocess.call(XELATEX_PATH + " --shell-escape -synctex=1 -interaction=nonstopmode sample.tex", shell=True)
+        bottle.response.content_type = 'application/pdf'
+        # bottle.response.content_type = 'text/plain'
+        # print("returning the sample.tex")
+        return bottle.static_file("sample.pdf", root=tempdir)
 
 
 if __name__ == '__main__':
