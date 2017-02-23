ba2b01cc588e0ea3fe183fc6640f4e5c341df29f
Miloššš Korenčiak <milos-korenciak@users.noreply.github.com>
2017-02-23 15:34:56 +0100

    updated worker for test of worker long run

--- worker.py
+++ worker.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+import time
+import datetime
+import random
+i = ranodm.random()
+while 1:
+    print("Hello worker %s on %s!"%(i, datetime.datetime.now()))
+    time.sleep(30)
