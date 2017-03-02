68dfbe3b37cd8b6b26ae1605098ba781dcb1e7e8
babbbrak_poradny <babbbrak_poradny@inmail.sk>
2017-03-02 12:51:00 +0100

    merged default example with recommendation by gist

--- worker.py
+++ worker.py
@@ -1,5 +1,5 @@
 import os
-import redis
+from redis import Redis
 import time
 import datetime
 import random
@@ -8,7 +8,7 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-
+redis = Redis()
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         worker = Worker(map(Queue, listen))
         worker.work()
 
-#i = random.random()
-#while 1:
-#    print("Hello worker %s on %s!"%(i, datetime.datetime.now()))
-#    time.sleep(30)
+i = random.random()
+while 1:
+    print("Hello worker {} on {}!".format(i, datetime.datetime.now()))
+    time.sleep(60)
