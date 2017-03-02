cdcf9dc4bbedabf9eb3b0d588d7d5dd8210f21dc
Miloššš Korenčiak <milos-korenciak@users.noreply.github.com>
2017-03-02 14:52:37 +0100

    test of max run time of worker

--- worker.py
+++ worker.py
@@ -10,13 +10,16 @@ listen = ['default']
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 redis = Redis()
 conn = redis.from_url(redis_url)
+start_time = datetime.datetime.now()
 
 if __name__ == '__main__':
+    i = random.random()
+    while 1:
+        print("Hello worker {} running {} at {}!".format(i,
+              datetime.datetime.now() - start_time, datetime.datetime.now()))
+        time.sleep(60)
+
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
 
-i = random.random()
-while 1:
-    print("Hello worker {} on {}!".format(i, datetime.datetime.now()))
-    time.sleep(60)
