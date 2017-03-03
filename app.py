7da66a411d5a0f1cc6355d05e0b63be12012426a
babbbrak_poradny <babbbrak_poradny@inmail.sk>
2017-03-03 14:58:29 +0100

    added Py3 compatibility imports

--- app.py
+++ app.py
@@ -4,6 +4,11 @@
 """The main app running automatic reporting
 @author: Milos.Korenciak@solargis.com"""
 
+
+from __future__ import print_function  # Python 2 vs. 3 compatibility --> use print()
+from __future__ import division  # Python 2 vs. 3 compatibility --> / returns float
+from __future__ import unicode_literals  # Python 2 vs. 3 compatibility --> / returns float
+from __future__ import absolute_import  # Python 2 vs. 3 compatibility --> absolute imports
 import bottle
 import datetime
 import db
