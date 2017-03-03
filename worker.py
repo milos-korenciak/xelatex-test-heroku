60969da953e08ae59906684b9b6f1e64a34698de
babbbrak_poradny <babbbrak_poradny@inmail.sk>
2017-03-03 13:28:19 +0100

    more stable worker - performing all stages sequentially = optimization +focus on 1 task

--- worker.py
+++ worker.py
@@ -1,19 +1,32 @@
-import os
 import db
+import time
 from app import process_request_raw_to_2nd_level, process_request_2nd_level_to_tex_raw, \
-    process_tex_raw_to_pdf_raw, process_pdf_raw_to_pdf_signed, TempDirContext
+    process_tex_raw_to_pdf_raw, process_pdf_raw_to_pdf_signed, TempDirContext, PdfCreationException
 
 
 if __name__ == '__main__':
     while 1:
         task = db.PdfCreation.get_locked_task()
+        if task is None:
+            print("going to sleep 20 sec")
+            time.sleep(20)
+            continue
         with TempDirContext() as tempdir:
-            process_request_raw_to_2nd_level(task, tempdir=tempdir)
-            process_request_2nd_level_to_tex_raw(task, tempdir=tempdir)
-            process_tex_raw_to_pdf_raw(task, tempdir=tempdir)
-            process_pdf_raw_to_pdf_signed(task, tempdir=tempdir)
-
-
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+            print("tempdir: {}, task.id {}".format(tempdir, task.pdf_creation_id))
+            try:
+                process_request_raw_to_2nd_level(task, tempdir=tempdir)
+            except PdfCreationException as e:
+                print(e)
+            try:
+                process_request_2nd_level_to_tex_raw(task, tempdir=tempdir)
+            except PdfCreationException as e:
+                print(e)
+            try:
+                process_tex_raw_to_pdf_raw(task, tempdir=tempdir)
+            except PdfCreationException as e:
+                print(e)
+            try:
+                process_pdf_raw_to_pdf_signed(task, tempdir=tempdir)
+            except PdfCreationException as e:
+                print(e)
+            print("end of task ;-)")
