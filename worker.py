e091a6516089a6c67d2532fd354455f393b82224
babbbrak_poradny <babbbrak_poradny@inmail.sk>
2017-03-03 11:47:49 +0100

    worker update

--- worker.py
+++ worker.py
@@ -1,15 +1,19 @@
 import os
+import db
+from app import process_request_raw_to_2nd_level, process_request_2nd_level_to_tex_raw, \
+    process_tex_raw_to_pdf_raw, process_pdf_raw_to_pdf_signed, TempDirContext
 
-import redis
-from rq import Worker, Queue, Connection
-
-listen = ['high', 'default', 'low']
-
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-
-conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
+    while 1:
+        task = db.PdfCreation.get_locked_task()
+        with TempDirContext() as tempdir:
+            process_request_raw_to_2nd_level(task, tempdir=tempdir)
+            process_request_2nd_level_to_tex_raw(task, tempdir=tempdir)
+            process_tex_raw_to_pdf_raw(task, tempdir=tempdir)
+            process_pdf_raw_to_pdf_signed(task, tempdir=tempdir)
+
+
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
