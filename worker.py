cb68b9d0d0d106d08d98f380ae5eca9145386579
babbbrak_poradny <babbbrak_poradny@inmail.sk>
2017-03-03 14:58:14 +0100

    added Py3 compatibility imports

--- worker.py
+++ worker.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Automatic reports worker
+@author: Milos.Korenciak@Solargis.com"""
+
+from __future__ import print_function  # Python 2 vs. 3 compatibility --> use print()
+from __future__ import division  # Python 2 vs. 3 compatibility --> / returns float
+from __future__ import unicode_literals  # Python 2 vs. 3 compatibility --> / returns float
+from __future__ import absolute_import  # Python 2 vs. 3 compatibility --> absolute imports
 import db
 import time
 from app import process_request_raw_to_2nd_level, process_request_2nd_level_to_tex_raw, \
